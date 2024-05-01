5f363fccaa0a01a0d79190cf38f985e940358dcb
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-05-01 01:20:09 +0000

    done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,6 +11,7 @@ class ActorsController < ApplicationController
     redirect_to("/actors")
   end
 
+  
   def destroy
     the_id = params.fetch("an_id")
     matching_records = Actor.where({:id => the_id})
@@ -19,6 +20,21 @@ class ActorsController < ApplicationController
   
     redirect_to("/actors")
   end
+
+
+  def update
+    the_actor_id = params.fetch("the_id")
+    the_actor = Actor.where(:id => the_actor_id).first
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+    the_actor.save
+
+    redirect_to("/actors")
+  end
+
+
 
   def index
     matching_actors = Actor.all
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,43 @@
 class DirectorsController < ApplicationController
+  def create
+    #Parameters: {"the_name"=>"", "the_dob"=>"", "the_bio"=>"", "the_image"=>""}
+    a = Director.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+    a.save
+
+    redirect_to("/directors")
+  end
+
+  
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_records = Director.where({:id => the_id})
+    the_director = matching_records.at(0)
+    the_director.destroy
+  
+    redirect_to("/directors")
+  end
+
+
+  def update
+    the_director_id = params.fetch("the_id")
+    the_director = Director.where(:id => the_director_id).first
+    the_director.name = params.fetch("the_name")
+    the_director.dob = params.fetch("the_dob")
+    the_director.bio = params.fetch("the_bio")
+    the_director.image = params.fetch("the_image")
+    the_director.save
+
+    redirect_to("_directors")
+  end
+
+
+
+
+
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
